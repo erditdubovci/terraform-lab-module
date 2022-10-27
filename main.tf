cc1b13623166c58d25ae000bbe7e59942f6bcd59
erditdubovci <dubovcierdit@gmail.com>
2022-10-27 21:19:33 +0200

    provider

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
+module "user-erdit-module" {
+  source  = "app.terraform.io/erditdubovci/user-erdit-module/iam"
+  version = "1.0.1"
+  # insert required variables here
 
+  user_name = "erdit-user"
+  policy_name = "erdit-policy"
+  policy_document = {
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*", "iam:GetAccountPasswordPolicy", "elasticloadbalancing:DescribeLoadBalancers"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:ChangePassword"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  }
+}
