eece06c94610224926642730d64858102627e7d9
erditdubovci <dubovcierdit@gmail.com>
2022-11-03 19:40:21 +0100

    Defined ACM Certificate and Route53 Record

--- route53.tf
+++ route53.tf
@@ -18,7 +18,7 @@ resource "aws_route53_record" "www" {
   
   alias {
     name                   = data.aws_lb.this.dns_name
-    zone_id                = data.aws_route53_zone.selected.zone_id
+    zone_id                = data.aws_lb.this.zone_id
     evaluate_target_health = true
   }
 }
