ac834c8091f8b33d6a2cb9f85d1f8d980dfacef5
erditdubovci <dubovcierdit@gmail.com>
2022-11-03 20:08:57 +0100

    fixed code

--- route53.tf
+++ route53.tf
@@ -1,14 +1,14 @@
 data "aws_route53_zone" "selected" {
-  name         = "apstellar.training"
+  name         = "appstellar.training"
   private_zone = false
 }
 
 data "aws_lb" "this" {
-    tags = {
-        "ingress.k8s.aws/resources" = "LoadBalancer",
-        "ingress.k8s.aws/stack"     = "default",
-        "elbv2.k8s.aws/cluster"     = "my-eks-cluster"
-    }
+  tags = {
+    "ingress.k8s.aws/resource" = "LoadBalancer",
+    "ingress.k8s.aws/stack"    = "default",
+    "elbv2.k8s.aws/cluster"    = "my-eks-cluster"
+  }
 }
 
 resource "aws_route53_record" "www" {
